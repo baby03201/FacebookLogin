b8d133626cc29d7690afe49ac8e7d9417a50a465
HuiQin <netccentricios@iOSs-MacBook-Air.local>
2014-08-08 12:12:36 +0800

    Initial Commit

---
+++ FacebookLoginIOS7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookLoginIOS7
+//
+//  Created by Ng Hui Qin on 8/8/14.
+//  Copyright (c) 2014 huiqintest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookLoginIOS7Tests/FacebookLoginIOS7Tests.m
@@ -0,0 +1,34 @@
+//
+//  FacebookLoginIOS7Tests.m
+//  FacebookLoginIOS7Tests
+//
+//  Created by Ng Hui Qin on 8/8/14.
+//  Copyright (c) 2014 huiqintest. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FacebookLoginIOS7Tests : XCTestCase
+
+@end
+
+@implementation FacebookLoginIOS7Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
